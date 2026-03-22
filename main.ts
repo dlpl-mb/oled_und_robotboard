196da9fed383388c0f032091eacc447aaba7dca8
Denken lernen - Probleme lösen <83607557+dlpl-mb@users.noreply.github.com>
2026-03-23 00:13:23 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,10 +7,12 @@ MuseOLED.clear()
 MuseOLED.writeStringNewLine("Alois Bachinger")
 MuseOLED.newLine()
 MuseOLED.writeStringNewLine("Temperatur: 12 grad")
+robotbit.MotorRun(robotbit.Motors.M1A, 161)
 basic.forever(function () {
     basic.pause(2000)
     MuseOLED.clear()
     basic.pause(2000)
+    robotbit.MotorRun(robotbit.Motors.M1A, 138)
     MuseOLED.writeNumNewLine(sonar.ping(
     DigitalPin.P2,
     DigitalPin.P12,
@@ -22,4 +24,5 @@ basic.forever(function () {
     DigitalPin.P8,
     PingUnit.Centimeters
     ))
+    robotbit.MotorRun(robotbit.Motors.M1A, -108)
 })
